07830e6a656691f6713c2ed4ec3fe45c03479e65
syang019 <stanley@stanleycyang.com>
2015-07-27 22:03:37 -0700

    Added additional links

--- js/app.js
+++ js/app.js
@@ -3,15 +3,24 @@ import Router from 'react-router';
 import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
 
 import LoginHandler from './components/Login.js';
+import SignupHandler from './components/Signup.js';
 
 class App extends React.Component{
   render() {
     return (
       <div className="nav">
+        <h1>Hello world!</h1>
         <Link to="app">Home</Link>
         <Link to="login">Login</Link>
+        <Link to="signup">Signup</Link>
+          <footer>
+            <ul>
+                <li>Click me</li>
+                <li>A link</li>
+                <li>Another</li>
+            </ul>
+          </footer>
 
-        {/* this is the importTant part */}
         <RouteHandler/>
       </div>
     );
@@ -21,6 +30,7 @@ class App extends React.Component{
 let routes = (  
   <Route name="app" path="/" handler={App}>
     <Route name="login" path="/login" handler={LoginHandler}/>
+    <Route name="signup" path="/signup" handler={SignupHandler} />
   </Route>
 );
 
---
+++ js/components/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Signup extends React.Component{
+    render(){
+        return (
+            <div>Sign up for our app</div>
+        );
+    }
+
+};
+
+export default Signup;
